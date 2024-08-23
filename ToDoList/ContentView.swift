0628cbcd40265a721e0f87318aff6ffa6a48c9d5
Sreeni Paloliyil <sreenipaloliyil1988@gmail.com>
2024-08-22 23:19:21 -0500

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Sreeni Paloliyil on 8/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sreeni Paloliyil on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
